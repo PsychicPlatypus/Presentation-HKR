991da07521f7758e76ef0df9b64f9de9a1f0e791
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2023-08-25 16:12:31 +0200

    init commit

---
+++ lib/presentation.ex
@@ -0,0 +1,18 @@
+defmodule Presentation do
+  @moduledoc """
+  Documentation for `Presentation`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Presentation.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
